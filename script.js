88ce700fccbb903379fbbe002d5e402290c61023
Chaitanya_E <144553457+Chaitanya-dav@users.noreply.github.com>
2025-08-06 20:21:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let boxELement = document.getElementsByClassName("box");
+boxELement
+
+
+let list = ["blue","green","yellow","red","pink"];
+
+
+for(let i = 0;i<5;i++){
+boxELement.style.backgroundcolor = list[i];
+}
